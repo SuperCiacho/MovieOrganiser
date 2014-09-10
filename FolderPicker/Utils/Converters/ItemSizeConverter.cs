330d87f4ec98eb4b2ed3b93ca3937203f01b38d8
Bartosz <bartosznovak@gmail.com>
2014-09-10 21:55:44 +0200

    YearPicker enhancements

---
+++ FolderPicker/Utils/Converters/ItemSizeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace YorgiControls.Utils.Converters
+{
+    public class ItemSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dim = (double) value;
+            int div = 1;
+            if ((string) parameter == "h") div = 3;
+            if ((string) parameter == "w") div = 4;
+            return Math.Floor(dim/div);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FolderPicker/ViewModels/ViewModelYearPicker.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace YorgiControls.ViewModels
+{
+    public class ViewModelYearPicker : ViewModelBase
+    {
+        private int minYear = 1900;
+        private int maxYear = 2085;
+        private int page = 1;
+        private int pageCount;
+        private int elementsCount = 12;
+        private string header;
+        private IEnumerable<string> dateItems;
+        private bool isYearSelectionMode;
+
+        private ICommand leftArrowCommand;
+        private ICommand rightArrowCommand;
+        private ICommand itemSelectedCommand;
+        private ICommand headerClickCommand;
+
+        public ViewModelYearPicker()
+        {
+            this.Header = "Select decade...";
+
+            CalculateDecadePeriods();
+
+            DateItems = DecadeItems;
+        }
+
+        #region Properties
+
+        #region Commands
+        public ICommand LeftArrowCommand
+        {
+            get
+            {
+                return leftArrowCommand ??
+                       (this.leftArrowCommand = new RelayCommand(() =>
+                       {
+                           if (this.page > 1) this.Page--;
+                       },
+                       () => page > 1));
+            }
+        }
+        public ICommand RightArrowCommand
+        { //TODO Fix CanExecute condition
+            get
+            {
+                return rightArrowCommand ?? (this.rightArrowCommand = new RelayCommand(() =>
+                {
+                    if (this.page < this.pageCount) this.Page++;
+
+                }, () => page == 1));
+            }
+        }
+        public ICommand ItemSelectedCommand
+        {
+            get { return itemSelectedCommand ?? (this.itemSelectedCommand = new RelayCommand<string>(s =>
+            {
+                if (isYearSelectionMode)
+                {
+                    Console.Beep();
+                }
+                else
+                {
+                    this.Header = "Select year...";
+                    ProduceYearItems(s);
+                }
+                
+            })); }
+        }
+        public ICommand HeaderClickCommand
+        {
+            get
+            {
+                return headerClickCommand ?? (this.headerClickCommand = new RelayCommand(() =>
+                {
+                    if (isYearSelectionMode)
+                    {
+                        isYearSelectionMode = false;
+                        this.Header = "Select decade...";
+                        DateItems = DecadeItems;
+                    }
+                }));
+            }
+        }
+
+        #endregion
+
+        public int MinYear
+        {
+            get { return minYear; }
+            set { minYear = value; }
+        }
+        public int MaxYear
+        {
+            get { return maxYear; }
+            set { maxYear = value; }
+        }
+        public int ElementsCount
+        {
+            get { return elementsCount; }
+            set { elementsCount = value; }
+        }
+
+        public string Header
+        {
+            get { return this.header; }
+            set
+            {
+                if (value == this.header) return;
+                this.header = value;
+                RaisePropertyChanged("Header");
+            }
+        }
+
+        public int Page
+        {
+            get { return this.page; }
+            set
+            {
+                if (this.page == value) return;
+                this.page = value;
+                RaisePropertyChanged("DateItems");
+            }
+        }
+
+        public IEnumerable<string> DateItems
+        {
+            get
+            {
+                return dateItems.Skip(12 * (page - 1));
+            }
+            set
+            {
+                this.dateItems = value;
+                RaisePropertyChanged("DateItems");
+            }
+        }
+
+        private IList<string> DecadeItems { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        private void CalculateDecadePeriods()
+        {
+            var period = DateTime.Now.Year - MinYear;
+            this.pageCount = (period + elementsCount - 1) / elementsCount;
+
+            DecadeItems = new List<string>();
+
+            for (var year = minYear; year < maxYear; year += 10)
+            {
+                var endYear = year + 9;
+                DecadeItems.Add(string.Format("{0}-\n{1}", year, endYear > maxYear ? maxYear : endYear));
+            }
+        }
+
+        private void ProduceYearItems(string param)
+        {
+            var years = param.Split('-').Select(int.Parse).ToArray();
+            var yearsItems = new List<string>();
+            for (var year = years[0] - 1; year <= years[1] + 1; year++)
+            {
+                yearsItems.Add(year.ToString(CultureInfo.InvariantCulture));
+            }
+            DateItems = yearsItems;
+            isYearSelectionMode = true;
+        }
+
+        #endregion
+
+    }
+}
